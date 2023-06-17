7db4c35c63c560ccf38d3ab7dc4d7e32b89fb102
Williamsunnyx <117774210+Williamsunnyx@users.noreply.github.com>
2023-06-17 18:34:02 +0100

    Add files via upload

--- thenigerian.js
+++ thenigerian.js
@@ -3,8 +3,9 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("openNav").style.display="none"
     
-
 }
+
+
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
@@ -19,6 +20,7 @@ function removeNav(){
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("openNav").style.display="block"
+
     
 }
 
@@ -33,9 +35,8 @@ function removeNav(){
        this.document.getElementById("dropDown").style.display="none"
         
        }
-  
-        
 
+ 
        
        up.classList.toggle("goup",this.window.scrollY > 900);
        if ("goup",this.window.scrollY > 900)
@@ -90,3 +91,114 @@ function closedrop(){
 
 }
 
+function showdropdownsmall(){
+  document.getElementById("small-dropdown").style.display="block"
+  
+}
+
+
+function turn(){
+var switchButton = document.getElementById("switch")
+var change = document.getElementById("bdy");
+var links = document.querySelectorAll('#bdy a,#bdy b,#bdy span,button,.numbers');
+
+switchButton.addEventListener('click', function(){
+  if (change.style.backgroundColor == 'white') {
+    change.style.backgroundColor = '';
+    document.body.style.color="white"
+
+    for (var i = 0; i < links.length; i++){
+      links[i].style.color = '';
+    }
+  }else {
+    change.style.backgroundColor = 'white';
+    for (var i =0; i < links.length; i++){
+      links[i].style.color ='black';
+      document.body.style.color="black"
+      
+      
+    }
+  }
+});
+
+}
+
+
+function gosearch(){
+  
+var searchInput = document.getElementById('searchInput');
+var content = document.getElementById('bdy');
+
+searchInput.addEventListener('input', function(){
+  var searchValue = searchInput.ariaValueMax.trim();
+  removeHighlight();
+
+  if (searchValue.length > 0) {
+    var similarLetters = findsimilarLetter(searchValue);
+    highlightLetters(similarLetters);
+  }
+});
+
+function removeHighlight() {
+  var highlightedElement = content.querySelectorAll('.highlight');
+  highlightedElement.forEach(function(element) {
+ element.classList.remove('highlight');
+
+  });
+}
+
+function findsimilarLetter(searchValue) {
+  var allText = content.textContent;
+  var similarLetters = [];
+
+  for (var i =0; i < allText.length; i++) {
+    if (searchValue.includes(allText[i])){
+      similarLetters.push(allText[i]);
+
+    }
+  }
+
+  return similarLetters;
+}
+
+function highlightLetters(letters) {
+  for (var i =0; i < letters.length; i++){
+    var regex = new RegExp(letters[i], 'gi');
+    content.innerHTML = content.innerHTML.replace(regex, function(match){
+      return '<span class ="highlight">' + match + '<span';
+    });
+  }
+}
+
+}
+
+
+// FOR DATE
+function displayDate() {
+  var currentDate= new Date();
+  
+  var day= currentDate.getDate();
+  var month= currentDate.getMonth() + 1; // months are zero based
+  var year= currentDate.getFullYear();
+
+  var dateString= day + "/" + month + "/" + year
+
+  document.getElementById("date").innerHTML= dateString;
+}
+
+function checkWeekend() {
+  var currentDate = new Date ();
+  var dayOfWeek = currentDate.getDate();
+  if (dayOfWeek === 0 || dayOfWeek === 6) {
+    document.getElementById("weekend").innerHTML = "it's the weekend!";
+
+  }else{
+
+    document.getElementById("weekend").innerHTML = "it's a weekday"
+  }
+}
+
+// DISPLAY THE DATE AND CHECK IF ITS THE WEEKEND IMMEDIATELY
+
+displayDate();
+checkWeekend();
